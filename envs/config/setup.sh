ad89b66621b5e9e248dd301324932dddbabef739
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-06 18:32:53 +0900

    :memo: プライベートIP取得追加

--- envs/config/setup.sh
+++ envs/config/setup.sh
@@ -82,6 +82,8 @@ chmod 640 /etc/frr/frr.conf
 ########################################
 # Strongswan settings
 ########################################
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+CGWPIP=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4`
 cat <<EOF > /etc/swanctl/conf.d/tgw.tf
 connections {
 
